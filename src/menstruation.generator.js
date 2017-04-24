1c47a8cef707d4868ff6cdecf054f9350358d7c7
Dariush Alipour <drsh.alipour@gmail.com>
2017-04-24 17:38:39 +0430

    added base generator code

---
+++ src/menstruation.generator.js
@@ -0,0 +1,27 @@
+const Generator = function Generator(dependencies) {
+  const id = 'dariush-alipour.onecalendar.generator.menstruation';
+  const name = 'Menstruation';
+  const inputs = [
+    { id: 'start', label: 'When did your last period start?', type: 'date', comment: '' },
+    { id: 'periodLength', label: 'How many days did it last?', type: 'number', comment: '' },
+  ];
+  const helper = {
+    getAdapter: dependencies.getAdapter,
+    primaryAdapterId: dependencies.primaryAdapterId,
+  };
+
+  const generate = (config) => {
+    const since = new dependencies.OneDate(config.start, helper);
+    const till = new dependencies.OneDate(config.start, helper).offsetDay(config.periodLength);
+    const event = new dependencies.Event({
+      title: 'Period Days',
+      since,
+      till,
+    });
+    return event;
+  };
+
+  return { id, name, inputs, generate };
+};
+
+exports.Generator = Generator;
---
+++ test/menstruation.generator.test.js
@@ -0,0 +1,43 @@
+/* eslint-disable no-unused-expressions */
+const expect = require('chai').expect;
+const Event = require('onecalendar-core').Event;
+const OneDate = require('onecalendar-core').OneDate;
+const GregorianAdapter = require('onecalendar-core').GregorianAdapter;
+const MenstruationGenerator = require('../src/menstruation.generator.js').Generator;
+
+const menstruationGenerator = new MenstruationGenerator(Event);
+
+describe('Basic Generator', () => {
+  describe('id', () => {
+    it('should return a string', () => {
+      expect(menstruationGenerator.id).to.be.a('string');
+    });
+  });
+
+  describe('name', () => {
+    it('should return a string', () => {
+      expect(menstruationGenerator.name).to.be.a('string');
+    });
+  });
+
+  describe('inputs', () => {
+    it('should return a non-empty array', () => {
+      expect(menstruationGenerator.inputs).to.not.be.empty;
+    });
+  });
+
+  describe('generate', () => {
+    it('should return an event object', () => {
+      const event = menstruationGenerator.generate({
+        title: 'Thanksgiving at grandma\'s house',
+        note: 'Wear good stuff, put some cologne and DO NOT talk much',
+        since: new OneDate({ year: 2017, month: 11, day: 23 },
+          {
+            getAdapter: () => new GregorianAdapter(),
+            primaryAdapterId: new GregorianAdapter().id,
+          }),
+      });
+      expect(event).to.be.an('object');
+    });
+  });
+});
